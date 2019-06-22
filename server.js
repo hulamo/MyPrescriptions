fdae5ad0fd3d48a60709efa9c00f07d47eed0188
hulamo <hugoladislao@gmail.com>
2019-06-21 20:17:15 -0600

    Modificaciones

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
     var env = require('dotenv').load()
     var exphbs = require('express-handlebars')
     var Handlebars = require('handlebars')
+    var PORT = process.env.PORT || 5000;
 
 
     //For BodyParser
@@ -68,7 +69,7 @@
 
 
 
-    app.listen(5000, function(err) {
+    app.listen(PORT, function(err) {
         if (!err)
             console.log("Site is live");
         else console.log(err)
